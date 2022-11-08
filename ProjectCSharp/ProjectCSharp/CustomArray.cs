85d0f5f8fd79dc2072bad9538ccec90c624ea4db
Steph V <s.villagordo@gmail.com>
2022-11-08 17:23:49 +0100

    First Part Array in C#

---
+++ ProjectCSharp/ProjectCSharp/CustomArray.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjectCSharp;
+
+public class CustomArray<T>
+{
+    public int Capacity { get; set; }
+    public int Size { get; set; }
+    
+    private int[]
+
+    public CustomArray(int x)
+    {
+        Capacity = x;
+    }
+
+    public bool IsEmpty()
+    {
+        return true;
+    }
+    
+}
