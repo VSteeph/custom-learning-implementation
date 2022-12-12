c27edc0f29ec5680d60ea43fe431b880dd6fc2a3
VSteeph <S.villagordo@gmail.com>
2022-12-12 15:05:13 +0100

    queue with linked List in C# Implementation

--- ProjectCSharp/ProjectCSharp/QUeues.cs
+++ ProjectCSharp/ProjectCSharp/QUeues.cs
@@ -1,6 +1,29 @@
 ﻿namespace ProjectCSharp;
 
-public class QUeues
+public class Queues<T>
 {
+    private CustomListNode<T> head = null;
     
+    public void Enqueue(T value)
+    {
+        var NewNode = new CustomListNode<T>(value);
+        NewNode.next = head;
+        head = NewNode;
+    }
+
+    public T Dequeue()
+    {
+        if (IsEmpty())
+        throw new NullReferenceException();
+        
+        var nodeToReturn = head;
+        head = head.next;
+        return nodeToReturn.item;
+    }
+
+    public bool IsEmpty()
+    {
+        return head is null ? true : false;
+    }
+
 }
--- ProjectCSharp/TestProject/QueuesTest.cs
+++ ProjectCSharp/TestProject/QueuesTest.cs
@@ -1,6 +1,52 @@
-﻿namespace TestProject;
+﻿using ProjectCSharp;
 
+namespace TestProject;
+
+[TestClass]
 public class QueuesTest
 {
+    [TestMethod]
+    public void ShouldCreateAQueue_WhenInstantiatingOne()
+    {
+        var sut = new Queues<int>();
+        Assert.IsNotNull(sut);
+    }
+    [TestMethod]
+    public void QueueSHouldBeEmpty_WhenCreated()
+    {
+        var sut = new Queues<int>();
+        Assert.IsTrue(sut.IsEmpty());
+    }
+    [TestMethod]
+    public void Queue_ShouldNotBeEmpty_WhenEnqueued()
+    {
+        var sut = new Queues<int>();
+        sut.Enqueue(1);
+        Assert.IsFalse(sut.IsEmpty());
+    }
     
+    [TestMethod]
+    public void Queue_ShouldReturnItem_WhenDequeueAndNotEmpty()
+    {
+        var sut = new Queues<int>();
+        sut.Enqueue(1);
+        Assert.AreEqual(1, sut.Dequeue());
+    }
+    
+    [TestMethod]
+    public void Queue_ShouldBeEmpty_WhenLastItemIsDequeue()
+    {
+        var sut = new Queues<int>();
+        sut.Enqueue(1);
+        sut.Dequeue();
+        Assert.IsTrue(sut.IsEmpty());
+    }
+    
+    [TestMethod]
+    public void Queue_ShouldThrowException_WhenDequeueIsUSedOnEmptyQueue()
+    {
+        var sut = new Queues<int>();
+        Assert.ThrowsException<NullReferenceException>(() => sut.Dequeue());
+    }
+
 }
